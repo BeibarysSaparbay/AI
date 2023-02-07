03448b59a4fefabde8f5592c1bf06ed796d09c4f
saparbaayevvv <68002429+BeibarysSaparbay@users.noreply.github.com>
2023-02-07 08:19:31 +0600

    Update sewie.py

--- football_match_predictors/sewie.py
+++ football_match_predictors/sewie.py
@@ -91,3 +91,128 @@ def get_goals_conceded(playing_stat):
     for i in range(2,39):
         GoalsConceded[i] = GoalsConceded[i] + GoalsConceded[i-1]
     return GoalsConceded
+
+def get_gss(playing_stat):
+    GC = get_goals_conceded(playing_stat)
+    GS = get_goals_scored(playing_stat)
+   
+    j = 0
+    HTGS = []
+    ATGS = []
+    HTGC = []
+    ATGC = []
+
+    for i in range(380):
+        ht = playing_stat.iloc[i].HomeTeam
+        at = playing_stat.iloc[i].AwayTeam
+        HTGS.append(GS.loc[ht][j])
+        ATGS.append(GS.loc[at][j])
+        HTGC.append(GC.loc[ht][j])
+        ATGC.append(GC.loc[at][j])
+        
+        if ((i + 1)% 10) == 0:
+            j = j + 1
+          playing_stat['HTGS'] = HTGS
+    playing_stat['ATGS'] = ATGS
+    playing_stat['HTGC'] = HTGC
+    playing_stat['ATGC'] = ATGC
+    
+    return playing_stat
+
+
+# Apply to each dataset
+playing_statistics_1 = get_gss(playing_statistics_1)
+playing_statistics_2 = get_gss(playing_statistics_2)
+playing_statistics_3 = get_gss(playing_statistics_3)
+playing_statistics_4 = get_gss(playing_statistics_4)
+playing_statistics_5 = get_gss(playing_statistics_5)
+playing_statistics_6 = get_gss(playing_statistics_6)
+playing_statistics_7 = get_gss(playing_statistics_7)
+playing_statistics_8 = get_gss(playing_statistics_8)
+playing_statistics_9 = get_gss(playing_statistics_9)
+playing_statistics_10 = get_gss(playing_statistics_10)
+playing_statistics_11 = get_gss(playing_statistics_11)
+playing_statistics_12 = get_gss(playing_statistics_12)
+playing_statistics_13 = get_gss(playing_statistics_13)
+playing_statistics_14 = get_gss(playing_statistics_14)
+playing_statistics_15 = get_gss(playing_statistics_15)
+playing_statistics_16 = get_gss(playing_statistics_16)
+playing_statistics_17 = get_gss(playing_statistics_17)
+playing_statistics_18 = get_gss(playing_statistics_18)
+
+
+def get_points(result):
+    if result == 'W':
+        return 3
+    elif result == 'D':
+        return 1
+    else:
+        return 0
+    
+
+def get_cuml_points(matchres):
+    matchres_points = matchres.applymap(get_points)
+    for i in range(2,39):
+        matchres_points[i] = matchres_points[i] + matchres_points[i-1]
+        
+    matchres_points.insert(column =0, loc = 0, value = [0*i for i in range(20)])
+    return matchres_points
+
+def get_matchres(playing_stat):
+    # Create a dictionary with team names as keys
+    teams = {}
+    for i in playing_stat.groupby('HomeTeam').mean().T.columns:
+        teams[i] = []
+
+    # the value corresponding to keys is a list containing the match result
+    for i in range(len(playing_stat)):
+        if playing_stat.iloc[i].FTR == 'H':
+            teams[playing_stat.iloc[i].HomeTeam].append('W')
+            teams[playing_stat.iloc[i].AwayTeam].append('L')
+        elif playing_stat.iloc[i].FTR == 'A':
+            teams[playing_stat.iloc[i].AwayTeam].append('W')
+            teams[playing_stat.iloc[i].HomeTeam].append('L')
+        else:
+            teams[playing_stat.iloc[i].AwayTeam].append('D')
+            teams[playing_stat.iloc[i].HomeTeam].append('D')
+            
+    return pd.DataFrame(data=teams, index = [i for i in range(1,39)]).T
+
+
+def get_agg_points(playing_stat):
+    matchres = get_matchres(playing_stat)
+    cum_pts = get_cuml_points(matchres)
+    HTP = []
+    ATP = []
+    j = 0
+    for i in range(380):
+        ht = playing_stat.iloc[i].HomeTeam
+        at = playing_stat.iloc[i].AwayTeam
+        HTP.append(cum_pts.loc[ht][j])
+        ATP.append(cum_pts.loc[at][j])
+
+        if ((i + 1)% 10) == 0:
+            j = j + 1
+            
+    playing_stat['HTP'] = HTP
+    playing_stat['ATP'] = ATP
+    return playing_stat
+
+playing_statistics_1 = get_agg_points(playing_statistics_1)
+playing_statistics_2 = get_agg_points(playing_statistics_2)
+playing_statistics_3 = get_agg_points(playing_statistics_3)
+playing_statistics_4 = get_agg_points(playing_statistics_4)
+playing_statistics_5 = get_agg_points(playing_statistics_5)
+playing_statistics_6 = get_agg_points(playing_statistics_6)
+playing_statistics_7 = get_agg_points(playing_statistics_7)
+playing_statistics_8 = get_agg_points(playing_statistics_8)
+playing_statistics_9 = get_agg_points(playing_statistics_9)
+playing_statistics_10 = get_agg_points(playing_statistics_10)
+playing_statistics_11 = get_agg_points(playing_statistics_11)
+playing_statistics_12 = get_agg_points(playing_statistics_12)
+playing_statistics_13 = get_agg_points(playing_statistics_13)
+playing_statistics_14 = get_agg_points(playing_statistics_14)
+playing_statistics_15 = get_agg_points(playing_statistics_15)
+playing_statistics_16 = get_agg_points(playing_statistics_16)
+playing_statistics_17 = get_agg_points(playing_statistics_17)
+playing_statistics_18 = get_agg_points(playing_statistics_18)
